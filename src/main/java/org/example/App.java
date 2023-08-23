fbc93e76773c6625ec0a5ac10fb4d4331ede3bbc
Oleksandra <oleksandrastratijcuk@gmail.com>
2023-08-23 12:24:11 +0300

    add time

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,10 +4,28 @@ package org.example;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        int number = 30;
+
+        long startTime = System.nanoTime();
+        FibonacciIteration iterativeResult = new FibonacciIteration();
+        long endTime = System.nanoTime();
+        long iterativeTime = endTime - startTime;
+
+        startTime = System.nanoTime();
+        FibonacciRecursion recursiveMemoResult = new  FibonacciRecursion();
+        endTime = System.nanoTime();
+        long recursiveMemoTime = endTime - startTime;
+
+        startTime = System.nanoTime();
+        FibonacciDynamic dynamicResult = new FibonacciDynamic();
+        endTime = System.nanoTime();
+        long dynamicTime = endTime - startTime;
+
+
+        System.out.println("Iterative Method: Fibonacci(" + number + ") = " + iterativeResult + " Time: " + iterativeTime + " ns");
+        System.out.println("Recursive with Memo Method: Fibonacci(" + number + ") = " + recursiveMemoResult + " Time: " + recursiveMemoTime + " ns");
+        System.out.println("Dynamic Method: Fibonacci(" + number + ") = " + dynamicResult + " Time: " + dynamicTime + " ns");
     }
 }
